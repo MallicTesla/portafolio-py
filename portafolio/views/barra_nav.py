4c5c1220ae9e911d8192e07afb401ed79b73aa0e
Mallic Tesla <el_mallic@hotmail.com>
2024-04-23 00:33:13 -0300

    la barra de navegacion esta completa

--- portafolio/views/barra_nav.py
+++ portafolio/views/barra_nav.py
@@ -3,7 +3,6 @@ from portafolio import portafolio
 
 
 def barra_nav() -> rx.Component:
-    S_about_id = portafolio.S_about.replace(" ", "-")
     S_technologies_id = portafolio.S_technologies.replace(" ", "-")
     S_projectss_id = portafolio.S_projects.replace(" ", "-")
     S_experience_id = portafolio.S_experience.replace(" ", "-")
@@ -11,15 +10,27 @@ def barra_nav() -> rx.Component:
     S_extras_id = portafolio.S_extras.replace(" ", "-")
 
 
-    return rx.hstack(
-        rx.link (portafolio.S_about, href = "#" + S_about_id),
-        rx.link (portafolio.S_technologies, href = "#" + S_technologies_id),
-        rx.link (portafolio.S_projects, href = "#" + S_projectss_id),
-        rx.link (portafolio.S_experience, href = "#" + S_experience_id),
-        rx.link (portafolio.S_training, href = "#" + S_training_id),
-        rx.link (portafolio.S_extras, href = "#" + S_extras_id),
+    return rx.center ( 
+        rx.hstack (
+            rx.link (portafolio.S_about, href = "#"),
+            rx.link (portafolio.S_technologies, href = "#" + S_technologies_id),
+            rx.link (portafolio.S_projects, href = "#" + S_projectss_id),
+            rx.link (portafolio.S_experience, href = "#" + S_experience_id),
+            rx.link (portafolio.S_training, href = "#" + S_training_id),
+            rx.link (portafolio.S_extras, href = "#" + S_extras_id),
 
-        justify_content = "center",
-        width = "100%",
-        spacing = "9",
+            justify_content = "center",
+            width = "100%",
+            spacing = "9",
+            top = 0,
+            # appearance = "#111414",
+            style={"background-color": "#111111", "padding": "10px", "z-index": "100"}
+        ),
+            width = "100%",
+            position = "fixed",
+            right = 0,
+            top = 0,
+            # appearance="inherit",
+            # style={"background-color": "red",}
     )
+
